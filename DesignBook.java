24eb2cd3caac681485f31d5db85a6926c260b12f
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-10-08 14:51:48 +0400

    Added DesignBook.java

---
+++ DesignBook.java
@@ -0,0 +1,22 @@
+public class DesignBook {
+    private String designField;
+    private boolean includesTemplates;
+
+    public DesignBook() {}
+
+    public String getDesignField() {
+        return designField;
+    }
+
+    public void setDesignField(String designField) {
+        this.designField = designField;
+    }
+
+    public boolean isIncludesTemplates() {
+        return includesTemplates;
+    }
+
+    public void setIncludesTemplates(boolean includesTemplates) {
+        this.includesTemplates = includesTemplates;
+    }
+}
